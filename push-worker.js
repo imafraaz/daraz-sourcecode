fb01627e8e8d5e3935ef52187c9a0cda937a405c
ABDULLAH AL AFRAAZ <64479658+imafraaz@users.noreply.github.com>
2021-10-24 02:47:10 +0600

    Add files via upload

---
+++ push-worker.js
@@ -0,0 +1,9 @@
+importScripts('https://aswpsdkeu.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+    defaultIcon: 'https://mobilecrm.accengage.com/images/logo_application/1511.png',
+    defaultTitle: 'Daraz Rocket internet',
+    defaultActionURL: 'https://www.daraz.pk/',
+    appKey: 'aATXTCQJSA6Gcn3oTbDU9Q',
+    token: 'MTphQVRYVENRSlNBNkdjbjNvVGJEVTlROnFXQVYwSjBzYlkzY29wLVdGajkwMlN5VW0tZmw0OWpvUEpyQVhxNzU5WU0',
+    vapidPublicKey: 'BPnqL43zhLkdfbcXnJ81dfEmZd3qDE4352_xF4qi_iwI6vFZQzr-vbTS1zPgc-OWwF-5vbk54Bx5WaEppzh6ylg'
+})
